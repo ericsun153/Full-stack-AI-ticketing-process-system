e6c3b9adb305ff4b618056e047e485823d9ad7f5
ericsun153 <2397286134@qq.com>
2025-11-15 15:17:41 -0500

    Import knowledge base api into actual application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/Home'
 import TicketList from './pages/TicketList'
 import CreateTicket from './pages/CreateTicket'
 import TicketDetail from './pages/TicketDetail'
+import KnowledgeBase from './pages/KnowledgeBase'
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/tickets" element={<TicketList />} />
         <Route path="/tickets/new" element={<CreateTicket />} />
-        <Route path="/tickets/id" element={<TicketDetail />} />
+        <Route path="/tickets/:id" element={<TicketDetail />} />
+        <Route path="/kb" element={<KnowledgeBase />} />
       </Routes>
     </AppLayout>
   )
